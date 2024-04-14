9152865c2368db76ae2b8b8e1b0bfb21e646c52f
Willslipknot <willslipknotpsico@gmail.com>
2024-04-14 15:13:44 -0600

    asd

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import morgan from "morgan";
 import authRoutes from './routes/auth.routes.js';
 import actRoutes from './routes/rutas.routes.js';
@@ -14,18 +15,18 @@ const allowedOrigins = [
     'https://raices-ashy.vercel.app',
     'http://localhost:5173',
     'https://raicesruralesf-4d6i.onrender.com'
-  ];
+];
 
-  app.use(cors({
+app.use(cors({
     origin: function(origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
     },
     credentials: true,
-  }));
+}));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(morgan('dev'));
@@ -37,5 +38,12 @@ app.use(condRoutes);
 app.use(vehiculoRoutes);
 app.use(ReservasRoutes);
 
+// Ruta para servir archivos estáticos de React
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Ruta de fallback para cualquier otra solicitud, sirve el archivo index.html de React
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 export default app;
